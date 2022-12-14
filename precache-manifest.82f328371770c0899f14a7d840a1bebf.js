a6240fc6f18834f9d807fffec96d02c889002837
Ayansaxena24 <ayan.saxena24@gmail.com>
2022-12-15 01:37:27 +0530

    Updates

---
+++ precache-manifest.82f328371770c0899f14a7d840a1bebf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "587b3410996a74135836603ef8bb8084",
+    "url": "/Keeper-App-3/index.html"
+  },
+  {
+    "revision": "9ab945d27882b14ed302",
+    "url": "/Keeper-App-3/static/css/main.5d79cda3.chunk.css"
+  },
+  {
+    "revision": "e6cbe565dec355a98270",
+    "url": "/Keeper-App-3/static/js/2.26daa70d.chunk.js"
+  },
+  {
+    "revision": "9ab945d27882b14ed302",
+    "url": "/Keeper-App-3/static/js/main.aa5a0c7c.chunk.js"
+  },
+  {
+    "revision": "c33fe7bdcd199ccee53d",
+    "url": "/Keeper-App-3/static/js/runtime-main.2e5614d3.js"
+  }
+]);
